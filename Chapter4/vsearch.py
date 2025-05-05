af9c8843fd6347228d1b74086b9ecf4534a25397
Anshul <thakuranshul25@yahoo.com>
2025-05-05 22:08:16 +0530

    Added Chapter4

--- Chapter4/vsearch.py
+++ Chapter4/vsearch.py
@@ -1,9 +1,9 @@
 def search4vowels(phrase: str) -> set:
-    """Return any vowels found in a supplied phrase."""
-    vowels = set('aeiou')
+    """Return any vowels found in a supplied word."""
+    vowels = set('aieou')
     return vowels.intersection(set(phrase))
 
-def search4letters(phrase: str, letters: str = 'aeiou') -> set:
-    """Return a set of the 'letters' found in 'phrase'."""
-    return set(letters).intersection(set(phrase))
 
+def search4letters(phrase: str, letters: str = 'aeiou') -> set:
+    """Return a set of the 'letters' found in the 'phrase'."""
+    return set(letters).intersection(set(phrase))
